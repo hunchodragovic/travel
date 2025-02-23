4c2be3abd1c1e86b78f8b98eeba37c98f06a1e27
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-02-23 19:11:08 +0100

    Dynamic Navbar Component with all it's links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <nav className="NavbarItems">
-      <h1 className="navbar-logo">Planet</h1>
+      <h1 className="navbar-logo">Planet Voyage</h1>
+      <div className="menu-icons">
+        <i
+          className={clicked ? "fas fa-times" : "fas fa-bars"}
+          onClick={() => setClicked(!clicked)}
+        ></i>
+      </div>
       <ul className="nav-menu">
-        <li>
-          <Link></Link>
-        </li>
+        {MenuItems.map((item, index) => (
+          <li key={index}>
+            <Link to={item.url} className={item.cName}>
+              <i className={item.icon}></i>
+              {item.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
